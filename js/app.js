0f6cd1b7348bf2aa38dc30ef75386dd5036a3801
Peter Matthews <pjdmatthews7@gmail.com>
2017-12-04 15:11:37 -0500

    Add bindings to HTML

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var viewModel = function(){
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
-    this.imgSrc = ko.obervable('img/434164568_fea0ad4013_z.jpg');
+    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('no stealing');
 
     this.incrementCounter = function() {
