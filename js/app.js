ff36fe8562a4d5cfd824e606ad13babb4050e25a
Peter Matthews <pjdmatthews7@gmail.com>
2017-12-04 17:27:41 -0500

    resolved some binding contexts

--- js/app.js
+++ js/app.js
@@ -1,14 +1,9 @@
-var viewModel = function(){
+var Cat = function(){
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('no stealing');
-    this.nickNames = ko.observableArray([
-        {nickName: "Bert"}, {nickName: "Ernie"}, {nickName: "BigBird"}]);
-
-    this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
-    };
+    this.nickNames = ko.observableArray(["Bert", "Ernie", "BigBird"]);
 
     this.catLevel = ko.computed(function(){
         if(this.clickCount() >= 15) {
@@ -19,6 +14,16 @@ var viewModel = function(){
             return "noob";
         }
     }, this);
+}
+
+var viewModel = function(){
+    var self = this;
+
+    this.currentCat = ko.observable(new Cat());
+
+    this.incrementCounter = function() {
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    };
 }
 
 ko.applyBindings(new viewModel());
